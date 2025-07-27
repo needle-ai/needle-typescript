29c679577406a9348bc823e71358804fea647648
Onur Eken <oeken@users.noreply.github.com>
2025-07-27 14:54:41 +0200

    Update base URL to needle.app (#3)

--- src/v1/constants.ts
+++ src/v1/constants.ts
@@ -1,2 +1,2 @@
-export const NEEDLE_URL = "https://needle-ai.com";
-export const NEEDLE_SEARCH_URL = "https://search.needle-ai.com";
+export const NEEDLE_URL = "https://needle.app";
+export const NEEDLE_SEARCH_URL = "https://search.needle.app";
